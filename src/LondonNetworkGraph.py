5d0acbb8a610e1ade4d77be520367fac0bce0743
nunoabgoncalves <nunoabgoncalves@gmail.com>
2026-05-29 18:25:39 +0100

    Acabei o visualize (ficou top!!). Tive de adicionar duas funções uma por causa de encotrar a estação e outra por causa da cor da linha

--- src/LondonNetworkGraph.py
+++ src/LondonNetworkGraph.py
@@ -26,9 +26,6 @@ class EdgeLine(Edge):
         return self.get_vs()
 
 def search_station(station_id, stations=None):
-    if stations is None:
-        stations = LondonNetworkGraph.stations()
-    stations = sorted(stations, key=lambda s: int(s[0]))
     upper_limit = len(stations) - 1
     lower_limit = 0
 
@@ -44,26 +41,12 @@ def search_station(station_id, stations=None):
     return None
 
 def line_color(connection, lines="include/lines.csv"):
-    lines_information = []
     with open(lines, newline='', encoding="utf-8") as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
-            if row:
-                lines_information.append(row)
-    lines_information = sorted(lines_information, key=lambda s: int(s[0]))
-    upper_limit = len(lines_information) - 1
-    lower_limit = 0
-
-    while lower_limit <= upper_limit:
-        pointer = (upper_limit + lower_limit) // 2
-        if int(lines_information[pointer][0]) == int(connection[2]):
-            return lines_information[pointer][2]
-        elif int(lines_information[pointer][0]) > int(connection[2]):
-            upper_limit = pointer - 1
-        else:
-            lower_limit = pointer + 1
-
+            if row and int(row[0]) == int(connection[2]):
+                return row[2]
     return None
 
 class LondonNetworkGraph(Graph):
@@ -111,7 +94,8 @@ class LondonNetworkGraph(Graph):
 
     def visualize(self):
         map = folium.Map(location=[51.5074, -0.1278], zoom_start=12, tiles="CartoDB positron")
-        for station in self.stations():
+        stations_sorted = sorted(self.stations(), key=lambda s: int(s[0]))
+        for station in stations_sorted:
             folium.CircleMarker(
                 location=[station[1], station[2]],
                 radius=5,
@@ -124,8 +108,8 @@ class LondonNetworkGraph(Graph):
             ).add_to(map)
 
         for connection in self.connections():
-            station_one = search_station(connection[0], self.stations())
-            station_two = search_station(connection[1], self.stations())
+            station_one = search_station(connection[0], stations_sorted)
+            station_two = search_station(connection[1], stations_sorted)
             folium.PolyLine(
                 locations=[[float(station_one[1]), float(station_one[2])],
                            [float(station_two[1]), float(station_two[2])]],
